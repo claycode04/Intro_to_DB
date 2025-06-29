47290099837486e82936e81429095da00cacfbed
claycode04 <yassdev4@gmail.com>
2025-06-29 16:02:12 +0100

    Fix table name in INSERT statements for consistency in SQL scripts

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-INSERT INTO Customers
+INSERT INTO Customer
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
